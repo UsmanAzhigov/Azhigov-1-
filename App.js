01293099526e5a4f5e867c91852df4e687568415
Archakov Dennis <hello@archakov.im>
2022-09-08 14:06:10 +0300

    Сделали урок #4

--- App.js
+++ App.js
@@ -1,90 +1,67 @@
-import React from 'react';
+import * as React from 'react';
 
-export default function App() {
-  const [phraseGenerator, setPhraseGenerator] = React.useState([]);
-  function AppHooks() {
-    const adjectivesArr = [
-      'абсолютный',
-      'адский',
-      'азартный',
-      'активный',
-      'ангельский',
-      'астрономический',
-      'баснословный',
-      'безбожный',
-      'безбрежный',
-      'безвозвратный',
-      'безграничный',
-      'бездонный',
-      'бездушный',
-      'безжалостный',
-      'замечательно',
-      'замечательный',
-      'записной',
-      'запредельный',
-      'заядлый',
-      'звериный',
-      'зверский',
-      'зеленый',
-      'злой',
-      'злостный',
-      'значительный',
-      'неоспоримый',
-      'неотразимый',
-      'неоценимый',
-      'непередаваемый',
-    ];
-    const nounsArr = [
-      'лгун',
-      'день',
-      'конь',
-      'олень',
-      'человек',
-      'программист',
-      'ребёнок',
-      'конец',
-      'город',
-      'дурак',
-    ];
+export default function Feedback() {
+  const MyBlock = () => {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+    const [comments, setComments] = React.useState([]);
 
-    const randomIndex1 = Math.floor(Math.random() * (adjectivesArr.length - 1));
-    const result1 = adjectivesArr[randomIndex1];
-    const randomIndex2 = Math.floor(Math.random() * (nounsArr.length - 1));
-    const result2 = nounsArr[randomIndex2];
-    const phrase = result1 + ' ' + result2;
-    setPhraseGenerator([phrase, ...phraseGenerator]);
-  }
-  const handleClear = () => {
-    setPhraseGenerator([]);
-  };
-  return (
-    <div className="wrapper">
-      <div className="block empty-block">
-        <div className="list">
-          {phraseGenerator.map((value) => {
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      const sms = [
+        {
+          fullName: name,
+          email: email,
+          createdAt: 'Thu Oct 14 2021 13:41:01',
+          text: message,
+        },
+      ];
+      // тут добавить в comments новый объект
+      setComments([...comments, ...sms]);
+      console.log('name:', name);
+      console.log('email:', email);
+      console.log('message', message);
+    }
+
+    return (
+      <div className="sending" onSubmit={handleSubmit}>
+        <div className="block">
+          {comments.map((obj) => {
             return (
-              <div className="block">
-                <h3>{value}</h3>
+              <div key={comments} style={{ border: '1px solid' }}>
+                <p>{obj.fullName}</p>
+                <p>{obj.email}</p>
+                <p>{obj.text}</p>
               </div>
             );
           })}
         </div>
-        <img
-          src="https://res.cloudinary.com/dfnhxiq6j/image/upload/v1640354646/ghost_lztiyl.png"
-          width="32px"
-          height="32px"
-          alt="Ghost"
-        />
-        <h2>Список фраз пустой</h2>
-        <p>Чтобы в этом списке появилась фраза, тебе необходимо нажать на кнопку “Сгенерировать”</p>
-      </div>
 
-      <button className="btn btn_generate" onClick={AppHooks}>
-        Сгенерировать
-      </button>
-      <button className="btn btn_clear" onClick={handleClear}>
-        Очистить
-      </button>
+        <h1>
+          <b>Обратная связь:</b>
+        </h1>
+        <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <textarea placeholder="Text" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button className="btn" onClick={handleSubmit}>
+          Отправить
+        </button>
+      </div>
+    );
+  };
+  return (
+    <div className="text">
+      <div>
+        <h1>Отзывы:</h1>
+        <div>
+          <img
+            className="image"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVNoIlXpNlqjo-W_HuEeXTHmmW1xM3CW_emc6bbH6FTK_3-I70jSAdGt4FMkS_jzyXhK4&usqp=CAU"></img>
+        </div>
+        <MyBlock></MyBlock>
+      </div>
     </div>
   );
 }
