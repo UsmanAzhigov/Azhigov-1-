e483f316974f0eb9ccd812efed96aaadf9c217ab
Archakov Dennis <hello@archakov.im>
2022-09-10 21:05:38 +0300

    Сделал урок 5

--- App.js
+++ App.js
@@ -1,65 +1,102 @@
 import * as React from 'react';
 
 export default function Feedback() {
-  const MyBlock = () => {
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [message, setMessage] = React.useState('');
-    const [comments, setComments] = React.useState([]);
+  const [fields, setFields] = React.useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-    function handleSubmit(event) {
-      event.preventDefault();
+  let [comments, setComments] = React.useState([]);
 
-      const sms = [
-        {
-          fullName: name,
-          email: email,
-          createdAt: 'Thu Oct 14 2021 13:41:01',
-          text: message,
-        },
-      ];
-      // тут добавить в comments новый объект
-      setComments([...comments, ...sms]);
-      console.log('name:', name);
-      console.log('email:', email);
-      console.log('message', message);
-      setName([]);
-      setEmail([]);
-      setMessage([]);
-    }
+  React.useEffect(() => {
+    localStorage.setItem('comments', JSON.stringify(comments));
+  }, [comments]);
 
-    return (
-      <div className="sending" onSubmit={handleSubmit}>
-        <div className="block">
-          {comments.map((obj) => {
-            return (
-              <div key={comments} style={{ border: '1px solid' }}>
-                <p>{obj.fullName}</p>
-                <p>{obj.email}</p>
-                <p>{obj.text}</p>
-              </div>
-            );
-          })}
-        </div>
+  function handleSubmit(event) {
+    event.preventDefault();
+    const date = new Date();
+    const sms = {
+      fullName: fields.name,
+      email: fields.email,
+      text: fields.message,
+      createdAt: date.toString(),
+    };
 
-        <h1>
-          <b>Обратная связь:</b>
-        </h1>
-        <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <textarea placeholder="Text" value={message} onChange={(e) => setMessage(e.target.value)} />
-        <button className="btn" onClick={handleSubmit}>
-          Отправить
-        </button>
-      </div>
+    //   1 сделать вывод настояще даты 2 добавлять в комментарий настоящее время (JS, как узнать время/дату) 3 сделать удаление комментариев 4
+
+    setComments([...comments, sms]);
+
+    setFields({
+      name: '',
+      email: '',
+      message: '',
+    });
+  }
+
+  const removeComment = (i) => {
+    setComments(
+      comments.filter((_, index) => {
+        return index !== i;
+      }),
     );
   };
+
   return (
     <div className="text">
       <div>
         <h1>Отзывы:</h1>
         <div></div>
-        <MyBlock></MyBlock>
+        <div className="sending" onSubmit={handleSubmit}>
+          <div className="block">
+            {comments.map((obj, index) => (
+              <div key={obj.email} style={{ border: '1px solid' }}>
+                <p>{obj.fullName}</p>
+                <p>{obj.email}</p>
+                <p>{obj.text}</p>
+                <p>{obj.createdAt}</p>
+                <button onClick={() => removeComment(index)}>X</button>
+              </div>
+            ))}
+          </div>
+
+          <h1>
+            <b>Обратная связь:</b>
+          </h1>
+          <input
+            placeholder="Name"
+            value={fields.name}
+            onChange={(e) => {
+              setFields({
+                ...fields,
+                name: e.target.value,
+              });
+            }}
+          />
+          <input
+            placeholder="Email"
+            value={fields.email}
+            onChange={(e) => {
+              setFields({
+                ...fields,
+                email: e.target.value,
+              });
+            }}
+          />
+          <textarea
+            placeholder="Text"
+            value={fields.message}
+            onChange={(e) => {
+              setFields({
+                ...fields,
+                message: e.target.value,
+              });
+            }}
+          />
+          <button className="btn" onClick={handleSubmit}>
+            Отправить
+          </button>
+        </div>
       </div>
     </div>
   );
